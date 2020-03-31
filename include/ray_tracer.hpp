37c937d474398d326f0c230386b06f5ada6e49ca
Veluga <albertboehm@hotmail.de>
2020-03-31 14:31:07 +0200

    Added vec3 class to header file.

--- include/ray_tracer.hpp
+++ include/ray_tracer.hpp
@@ -1,6 +1,32 @@
 #ifndef RAYTRACER_H
 #define RAYTRACER_H
 
+#include <iostream>
+
 void gen_simple_fading(int, int);
+
+class vec3 {
+  vec3();
+  vec3(double, double, double);
+
+  double x() const;
+  double y() const;
+  double z() const;
+
+  vec3 operator-() const;
+  double operator[](int i) const;
+  double &operator[](int i);
+
+  vec3 &operator+=(const vec3 &);
+  vec3 &operator*=(const double t);
+  vec3 &operator/=(const double t);
+
+  double length() const;
+  double length_squared() const;
+  void write_color(std::ostream &);
+
+public:
+  double e[3];
+};
 
 #endif
