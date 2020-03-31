49d88e8cdbdc6ea9d7017080aaeaa0c89402155b
Veluga <albertboehm@hotmail.de>
2020-03-31 17:32:18 +0200

    Added ray based fading.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,32 @@
 #include "ray_tracer.hpp"
 #include <iostream>
 
-int main() { ray r; }
+vec3 ray_color(const ray &r) {
+  vec3 unit_direction = unit_vector(r.direction());
+  auto t = 0.5 * (unit_direction.y() + 1.0);
+  return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+}
+
+int main() {
+  int image_width = 200;
+  int image_height = 100;
+
+  std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
+
+  vec3 lower_left_corner(-2.0, -1.0, -1.0);
+  vec3 horizontal(4.0, 0.0, 0.0);
+  vec3 vertical(0.0, 2.0, 0.0);
+  vec3 origin(0.0, 0.0, 0.0);
+
+  for (int i = image_height - 1; i >= 0; i--) {
+    std::cerr << "\nScanlines remaining: " << i << ' ' << std::flush;
+    for (int j = 0; j < image_width; j++) {
+      auto u = static_cast<double>(j) / image_width;
+      auto v = static_cast<double>(i) / image_height;
+      ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+      vec3 color = ray_color(r);
+      color.write_color(std::cout);
+    }
+  }
+  std::cerr << "\nDone.\n";
+}
