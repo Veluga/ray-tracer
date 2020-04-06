2b0dc388de483b73507b418add78d7f3d80ca619
Veluga <albertboehm@hotmail.de>
2020-04-06 17:22:09 +0200

    Enabled rendering of random scenes.

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 
 #include <cmath>
 #include <iostream>
+#include <vector>
 
 vec3 ray_color(const ray &r, hittable_list world, int depth) {
   hit_record hit;
@@ -28,10 +29,55 @@ vec3 ray_color(const ray &r, hittable_list world, int depth) {
   return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 }
 
+hittable_list random_scene() {
+  hittable_list world;
+
+  world.add(std::make_shared<sphere>(
+      vec3(0, -1000, 0), 1000,
+      std::make_shared<lambertian>(vec3(0.5, 0.5, 0.5))));
+
+  int i = 1;
+  for (int a = -11; a < 11; a++) {
+    for (int b = -11; b < 11; b++) {
+      auto choose_mat = random_double();
+      vec3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
+      if ((center - vec3(4, 0.2, 0)).length() > 0.9) {
+        if (choose_mat < 0.8) {
+          // diffuse
+          auto albedo = vec3::random() * vec3::random();
+          world.add(std::make_shared<sphere>(
+              center, 0.2, std::make_shared<lambertian>(albedo)));
+        } else if (choose_mat < 0.95) {
+          // metal
+          auto albedo = vec3::random(.5, 1);
+          auto fuzz = random_double(0, .5);
+          world.add(std::make_shared<sphere>(
+              center, 0.2, std::make_shared<metal>(albedo, fuzz)));
+        } else {
+          // glass
+          world.add(std::make_shared<sphere>(
+              center, 0.2, std::make_shared<dielectric>(1.5)));
+        }
+      }
+    }
+  }
+
+  world.add(std::make_shared<sphere>(vec3(0, 1, 0), 1.0,
+                                     std::make_shared<dielectric>(1.5)));
+
+  world.add(std::make_shared<sphere>(
+      vec3(-4, 1, 0), 1.0, std::make_shared<lambertian>(vec3(0.4, 0.2, 0.1))));
+
+  world.add(std::make_shared<sphere>(
+      vec3(4, 1, 0), 1.0, std::make_shared<metal>(vec3(0.7, 0.6, 0.5), 0.0)));
+
+  return world;
+}
+
 int main() {
-  const int image_width = 200;
-  const int image_height = 100;
-  const int samples_per_pixel = 100;
+  const int image_width = 800;
+  const int image_height = 400;
+  const int samples_per_pixel = 50;
   const int max_depth = 50;
   const auto aspect_ratio = double(image_width) / image_height;
 
@@ -42,25 +88,13 @@ int main() {
   vec3 vertical(0.0, 2.0, 0.0);
   vec3 origin(0.0, 0.0, 0.0);
 
-  hittable_list world;
-  world.add(std::make_shared<sphere>(
-      vec3(0, 0, -1), 0.5,
-      (std::make_shared<lambertian>(vec3(0.1, 0.2, 0.5)))));
-  world.add(std::make_shared<sphere>(
-      vec3(0, -100.5, -1), 100,
-      std::make_shared<lambertian>(vec3(0.8, 0.8, 0.0))));
-  world.add(std::make_shared<sphere>(
-      vec3(1, 0, -1), 0.5, std::make_shared<metal>(vec3(0.8, 0.6, 0.2), 0.3)));
-  world.add(std::make_shared<sphere>(vec3(-1, 0, -1), 0.5,
-                                     std::make_shared<dielectric>(1.5)));
-  world.add(std::make_shared<sphere>(vec3(-1, 0, -1), -0.45,
-                                     std::make_shared<dielectric>(1.5)));
+  auto world = random_scene();
 
-  vec3 lookfrom(3, 3, 2);
-  vec3 lookat(0, 0, -1);
+  vec3 lookfrom(13, 2, 3);
+  vec3 lookat(0, 0, 0);
   vec3 vup(0, 1, 0);
-  auto dist_to_focus = (lookfrom - lookat).length();
-  auto aperture = 2.0;
+  auto dist_to_focus = 10;
+  auto aperture = 0.1;
   camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
   for (int i = image_height - 1; i >= 0; i--) {
     std::cerr << "\nScanlines remaining: " << i << ' ' << std::flush;
